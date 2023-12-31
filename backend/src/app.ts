49f436c1bc4e13f59d1e2d36cd9c0cd5bf322c3e
VipulD <vipuld04@gmail.com>
2023-12-31 19:19:14 +0530

    cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,7 +12,7 @@ const app = express();
 // app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(
   cors({
-    origin: "https://smarty-openai-chatbot.vercel.app/",
+    origin: "https://smarty-openai-chatbot.vercel.app",
     credentials: true,
   })
 );
